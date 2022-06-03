d09417cb2d15a812b506a983aa819b46c73ccc5e
Fernanda Caetano de mattos bastos cunha <fcaetano@c2t1s5.42.rio>
2022-06-03 16:39:06 -0300

    striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,17 +6,36 @@
 /*   By: fcaetano <fernandacunha@id.uff.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 19:06:20 by fcaetano          #+#    #+#             */
-/*   Updated: 2022/06/02 19:22:49 by fcaetano         ###   ########.fr       */
+/*   Updated: 2022/06/03 15:53:53 by fcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_striteri(char *s, void(*f)(unsigned int, char*)
+void	ft_striteri(char *s, void(*f)(unsigned int, char*))
 {
 	unsigned int i;
 
+	i = 0;
 	while(s[i])
 	{
-		s[i] = f(i, str[i]);
+		f(i, s + i);
 		i++;	
 	}
 }
+
+/*
+void	ft_toupper(unsigned int i, char* c)
+{
+	i = 0;
+    if (*c >= 'a' && *c <= 'z')
+        *c -= 32;
+}
+#include <stdio.h>
+int main(void)
+{
+	char *str = "teste";
+
+	ft_striteri(str, ft_toupper);
+	printf("%s", str);
+	return (0);	
+}
+*/
